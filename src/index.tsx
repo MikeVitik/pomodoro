940532af584797797a3efbc623c3ea896430aa7f
MikeVitik <vitikm@gmail.com>
2024-02-23 18:53:07 +0300

    add deploy script

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/pomodoro">
         <App />
       </BrowserRouter>
     </Provider>
